11e623eb2b120917b4a8177ad9b2aacbfded3115
satem02 <safaktemel02@gmail.com>
2020-01-28 08:42:17 +0300

    Add index.ts for components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent, FooterComponent, HomeComponent } from './components';
+
 
 @NgModule({
   declarations: [
---
+++ src/app/components/index.ts
@@ -0,0 +1,3 @@
+export * from './header/header.component';
+export * from './home/home.component';
+export * from './footer/footer.component';

610850eecf5b08da08c86e8ef06aeffc5137c3e6
satem02 <safaktemel02@gmail.com>
2020-01-30 10:31:18 +0300

    export partial component to product module.

--- src/app/modules/product/product.module.ts
+++ src/app/modules/product/product.module.ts
@@ -6,13 +6,15 @@ import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { LayoutComponent } from './components/layout/layout.component';
+import { PartialComponent } from './components/partial/partial.component';
 
 
 @NgModule({
-  declarations: [HeaderComponent, HomeComponent, FooterComponent, LayoutComponent],
+  declarations: [HeaderComponent, HomeComponent, FooterComponent, LayoutComponent, PartialComponent],
   imports: [
     CommonModule,
     ProductRoutingModule
-  ]
+  ],
+  exports: [PartialComponent]
 })
 export class ProductModule { }

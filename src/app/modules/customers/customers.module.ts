38d2b999210b49894f871e42aaa85805cffaedd8
satem02 <safaktemel02@gmail.com>
2020-01-30 09:30:49 +0300

    Add first example module.

--- src/app/modules/customers/customers.module.ts
+++ src/app/modules/customers/customers.module.ts
@@ -3,10 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { CustomersRoutingModule } from './customers-routing.module';
 import { CustomersComponent } from './customers.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
 
 
 @NgModule({
-  declarations: [CustomersComponent],
+  declarations: [CustomersComponent, HeaderComponent, FooterComponent, HomeComponent],
   imports: [
     CommonModule,
     CustomersRoutingModule
